83886731e1011af37f6a3e05f7a29942891b6ed5
prathmesh <prathameshk990@gmail.com>
2024-10-24 21:15:56 +0530

    added Middle wares

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,39 @@
 const express = require("express");
+const {middle2} = require("./Middlewares/middle1");
+// const  = require("./Middlewares/middle1");
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT||5002;
+const PORT = process.env.PORT || 5002;
 
 const userRoutes = require("./Routes/userRoutes");
 const ConnectDB = require("./Configure/Database").DBConnect;
-app.use("/api/v1/user", userRoutes); 
 
+app.use("/api/v1/user", userRoutes);
+
+// app.use(middle1);
+app.use(middle2);
+
+app.get("/ride1", (req, res) => {
+  res.json({
+    message: "You can ride the ride 1",
+  });
+});
+
+app.get("/ride2", (req, res) => {
+  res.json({
+    message: "Yopu can Ride the ride 2",
+  });
+});
+
+app.get("/ride3", (req, res) => {
+  res.json({
+    message: "Yopu can Ride the ride 3",
+  });
+});
+
+// app.use((err, req, res, next) => {
+//   res.status(400).send("Something went wrong");
+// });
 ConnectDB();
 
 app.listen(PORT, () => {
